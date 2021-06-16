37e2d1d599e1c578ea7fe930872a8d3454e6de94
Jerry-glod <2041189779@qq.com>
2021-06-16 21:45:08 +0800

    简单结构，服务器与客户端的连接

---
+++ 网络/客户端与服务器连接（服务端）/客户端与服务器连接（服务端）/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace 客户端与服务器连接_服务端_
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //bind
+            IPAddress ipadr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint IPEnd = new IPEndPoint(ipadr, 8888);
+            listenfd.Bind(IPEnd);
+            //监听
+            listenfd.Listen(0);
+            Console.WriteLine("服务器启动成功");
+            while (true)
+            {
+                //Accept
+                Socket connectfd = listenfd.Accept();
+                Console.WriteLine("服务器accept");
+                //接收
+                byte[] readBuff = new byte[1024];
+                int count = connectfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+                Console.WriteLine("服务器接收" + readStr);
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+                connectfd.Send(sendBytes);
+            }
+        }
+        
+    }
+}
---
+++ 网络/客户端与服务端的连接(客户端)/Assets/Scenes/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    Socket socket;
+    public InputField InputField;
+    public Text text;
+
+    public void Connection()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect("127.0.0.1", 8888);
+    }
+    public void Send()
+    {
+        //send
+        string sendStr = InputField.text;
+        byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+        socket.Send(sendBytes);
+        Debug.Log(sendBytes);
+        //receive
+        byte[] readBuff = new byte[1024];
+        int count = socket.Receive(readBuff);
+        string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+        text.text = recvStr;
+        socket.Close();
+    }
+}
